172af6fa094a9ce983552ba83dc24b6d670e1c6a
j-farkas <jaredmfarkas@gmail.com>
2015-10-05 22:21:40 -0700

    Update movies.py

--- movies.py
+++ movies.py
@@ -6,11 +6,12 @@ class Movies(object):
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
+#Create list
 l = []
+#Add movies to list
 l.append(Movies("Ghostbusters","http://ia.media-imdb.com/images/M/MV5BMTkxMjYyNzgwMl5BMl5BanBnXkFtZTgwMTE3MjYyMTE@._V1_SX214_AL_.jpg","https://youtu.be/vntAEVjPBzQ"))
 l.append(Movies("Back to the Future","http://ia.media-imdb.com/images/M/MV5BMjA5NTYzMDMyM15BMl5BanBnXkFtZTgwNjU3NDU2MTE@._V1_SX214_AL_.jpg","https://youtu.be/qvsgGtivCgs"))
 l.append(Movies("Pulp Fiction","http://ia.media-imdb.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_SX214_AL_.jpg","https://youtu.be/s7EdQ4FqbhY"))
 l.append(Movies("The Silence of the Lambs","http://ia.media-imdb.com/images/M/MV5BMTQ2NzkzMDI4OF5BMl5BanBnXkFtZTcwMDA0NzE1NA@@._V1_SX214_AL_.jpg","https://youtu.be/lQKs169Sl0I"))
-#Since comments were listed as required, I wanted to take a minute to wonder if my movie choices dated me before calling the function
-
+#Call Function in the included file
 fresh_tomatoes.open_movies_page(l)
